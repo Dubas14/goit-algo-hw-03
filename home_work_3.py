ad0a5122645339992117dec1d4f2c55e69e7cf83
Dubas14 <ev.hanzha@gmail.com>
2024-06-30 21:36:29 +0300

    file commit

---
+++ home_work_3.py
@@ -0,0 +1,13 @@
+import re
+
+def normalize_phone(phone_number):
+    
+    phone_number = re.sub(r'\D', '', phone_number)
+    
+    if not phone_number.startswith('+'):
+        if phone_number.startswith('380'):
+            phone_number = '+' + phone_number
+        else:
+            phone_number = '+38' + phone_number
+    
+    return phone_number
